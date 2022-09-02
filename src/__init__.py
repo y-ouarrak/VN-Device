d80a8aa4299dd6182685b8854a48ba4bf2fd14ee
Yassine Ouarrak <y.ouarrak@outlook.com>
2022-09-02 10:16:21 +0100

    wip : delete subscribe

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,8 @@ from time import sleep
 from .utils import config
 from .core import client
 
-print(client)
-print(client.connect(config.get('mqtt_host'), config.get('mqtt_port')))
-client.subscribe('#')
+client.connect(config.get('mqtt_host'), config.get('mqtt_port'))
+
 with open('events.csv') as events:
     for row in events:
         print(config.get('mqtt_topic'))
